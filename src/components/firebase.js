e941c31dc16336068a5fab38923ed6d88f167561
Sushant <sushant.adhikari.619@gmail.com>
2021-04-11 21:26:48 +0545

    project setup and header component

---
+++ src/components/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDGqeRH2hXZ-wnJgSoRv0Warp-Fn2N_nkU",
+  authDomain: "slack-clone-s.firebaseapp.com",
+  projectId: "slack-clone-s",
+  storageBucket: "slack-clone-s.appspot.com",
+  messagingSenderId: "28475702014",
+  appId: "1:28475702014:web:25e9611dfe8b8b6492c3af"
+};
